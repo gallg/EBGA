7ae9f996e703d87d716fd93a7c6e0f510a68b87e
gallg <giuseppe.gallitto01@gmail.com>
2026-07-16 16:18:08 +0200

    [setup.py] version updated

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="EBGA",
-    version="0.1.3",
+    version="0.1.4",
     author="Giuseppe Gallitto",
     description="Evolutionary Based Gradient Alignment",
     long_description=long_description,
