ab415c3e419f3ea091022d49a242c9e64702d4f2
gallg <giuseppe.gallitto01@gmail.com>
2026-08-25 01:11:00 +0200

    [setup] version updated

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="EBGA",
-    version="0.2.3",
+    version="0.2.4",
     author="Giuseppe Gallitto",
     description="Evolutionary-Based Generative Adaptation",
     long_description=long_description,
